33815e8e95300ecfd56b804a5e860252951ccb17
Henrique <hcgv1@hotmail.com>
2026-04-23 14:15:02 -0300

    feat(income): add income model and repository

---
+++ finflow-income/src/main/java/com/finflow/income/repository/IncomeRepository.java
@@ -0,0 +1,14 @@
+package com.finflow.income.repository;
+
+import com.finflow.income.model.Income;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IncomeRepository extends MongoRepository<Income, String> {
+
+    List<Income> findByUserId(String userId);
+
+    Optional<Income> findByIdAndUserId(String id, String userId);
+}
