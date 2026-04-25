4c2dfa05e3684664bd57181d8ce3586934dc9ea8
Henrique <hcgv1@hotmail.com>
2026-04-25 16:28:34 -0300

    chore(gateway): enable gateway configuration property scanning

--- finflow-gateway/src/main/java/com/finflow/gateway/FinflowGatewayApplication.java
+++ finflow-gateway/src/main/java/com/finflow/gateway/FinflowGatewayApplication.java
@@ -2,7 +2,9 @@ package com.finflow.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class FinflowGatewayApplication {
 
@@ -10,4 +12,3 @@ public class FinflowGatewayApplication {
         SpringApplication.run(FinflowGatewayApplication.class, args);
     }
 }
-
