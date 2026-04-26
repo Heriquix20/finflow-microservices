0bfb2ce1e0d8368672234d0d66aa478c9c446e44
Henrique <hcgv1@hotmail.com>
2026-04-26 16:55:55 -0300

    feat(gateway): improve jwt request filtering

--- finflow-gateway/src/main/java/com/finflow/gateway/filter/JwtAuthenticationFilter.java
+++ finflow-gateway/src/main/java/com/finflow/gateway/filter/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
@@ -36,6 +37,11 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String path = exchange.getRequest().getURI().getPath();
+        HttpMethod method = exchange.getRequest().getMethod();
+
+        if (HttpMethod.OPTIONS.equals(method)) {
+            return chain.filter(exchange);
+        }
 
         if (isPublicPath(path)) {
             return chain.filter(exchange);
