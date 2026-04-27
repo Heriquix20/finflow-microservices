5596079fac13778ca686a71e03b18f450f06715c
Henrique <hcgv1@hotmail.com>
2026-04-27 14:44:46 -0300

    feat(auth): bootstrap auth microservice

---
+++ finflow-auth/src/main/java/com/finflow/auth/FinflowAuthApplication.java
@@ -0,0 +1,12 @@
+package com.finflow.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinflowAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinflowAuthApplication.class, args);
+    }
+}
